abc1b4de66b4efd8af19ec5c4d832576116e7335
Arindam Bose <abose@aurorasolar.com>
2017-07-21 17:13:55 -0700

    Comment

--- src/Stats.js
+++ src/Stats.js
@@ -1,5 +1,7 @@
 /**
  * @author mrdoob / http://mrdoob.com/
+ * @author arindam1993 - Forked to allow for multiple stats panels per `Stats` obj.
+ * 						(https://github.com/arindam1993/stats.js)
  */
 
 var Stats = function () {
